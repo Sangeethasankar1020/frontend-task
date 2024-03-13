8b3a34f60e3bdfd969a0cebcca2d86041b11b0c2
Sangeetha S <sangeethasankar474@gmail.com>
2024-03-13 16:55:03 +0530

    update'

--- js/data/data.js
+++ js/data/data.js
@@ -1,4 +1,27 @@
 // datatypes
+// primitive
 // string
-var name ="sangeetha";
-console.log(name,"my name");
+var name = "sangeetha";
+console.log(name, "my name");
+// number
+var mobileNo = 7639777260;
+console.log(mobileNo, "mobile no");
+// boolean
+var deactive = false;
+console.log(deactive, "active status");
+// null
+var myName = null;
+console.log(myName, "null status");
+// undefined
+var reg;
+console.log(reg, "red declare");
+// non primitive
+// object
+var employeeDate = {
+  sno: 123,
+  name: "santhosh",
+  role: "develloper",
+};
+console.log(employeeDate, "employe date");
+var course = ["data analyst", "development"];
+console.log(course, "course status");
