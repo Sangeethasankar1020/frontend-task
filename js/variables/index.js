d7d1f9a551dd9502300df4d26a8693d66eb4ab0d
SwethaSuresh-15 <swethasree1518@gmail.com>
2024-03-14 16:22:24 +0530

    update

---
+++ js/variables/index.js
@@ -0,0 +1,17 @@
+// variables
+// let it can  reassign
+let name;
+name="sangeetha";
+name="gayu";
+console.log(name,"let");
+
+//var both
+var num;
+num=20;
+num=10; 
+var num;
+console.log(num,"var");
+
+// const not             
+const place="pondy";
+console.log(place,"const")
