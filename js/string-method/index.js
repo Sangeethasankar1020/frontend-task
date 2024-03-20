8b78d0ca7d1645b9050cd23368c76e66edcaf4e8
SwethaSuresh-15 <swethasree1518@gmail.com>
2024-03-20 16:10:50 +0530

    update

---
+++ js/string-method/index.js
@@ -0,0 +1,13 @@
+//string method
+let country=" India";
+console.log(country.length,"length")
+
+// uppercase
+let course="mern satck";
+console.log(course.toUpperCase(),"uppercase")
+
+// lower case
+let welcome="hello WORLD"
+console.log(welcome.toLowerCase(),"lowercase")
+// indexof
+let 
