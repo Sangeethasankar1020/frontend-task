a1bf5da665d7bfcee3696784e56632262e249f81
SwethaSuresh-15 <swethasree1518@gmail.com>
2024-04-04 16:41:22 +0530

    update

--- js/star pattern loop/index.js
+++ js/star pattern loop/index.js
@@ -1,4 +1,4 @@
-//1.right triangle
+//1.left triangle
 let n = 5;
 let string = "";
 for (let i = 1; i <= n; i++) {
@@ -10,7 +10,7 @@ for (let i = 1; i <= n; i++) {
   }
   string += "\n";
 }
-console.log(string);
+console.log(string ,"left");
 //2.square star pattern
 let num = 5;
 let str = "";
@@ -37,3 +37,37 @@ for (let i = 0; i < row; i++) {
   star = star + "\n";
 }
 console.log(star, "hollow pattern");
+// 4.right triangle
+let nu = 5;
+let stri = "";
+for (let i = 1; i <= nu; i++) {
+  for (let j = 0; j < i; j++) {
+    stri += "*";
+  }
+  stri += "\n";
+}
+console.log(stri,"right angled triangle");
+// 5. downward triangle star pattern
+let m = 5;
+let st = "";
+for (let i = 0; i < m; i++) {
+  // printing star
+  for (let k = 0; k < m - i; k++) {
+    st += "*";
+  }
+  st += "\n";
+}
+console.log(st,"downward triangle");
+// 6. pyramid star pattern
+let r = 5;
+let s = "";
+for (let i = 1; i <= r; i++) {
+  for (let j = 1; j <= r - i; j++) {
+    s += " ";
+  }
+  for (let k = 0; k < 2 * i - 1; k++) {
+     s+= "*";
+  }
+  s += "\n";
+}
+console.log(s,"pyramid");
