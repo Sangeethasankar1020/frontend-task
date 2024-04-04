290d5e2521476e5978f96f1d227af17361ec1da8
Sangeetha S <sangeethasankar474@gmail.com>
2024-04-04 14:48:45 +0530

    update

--- js/star pattern loop/index.js
+++ js/star pattern loop/index.js
@@ -1,4 +1,4 @@
-//left triangle
+//1.right triangle
 let n = 5;
 let string = "";
 for (let i = 1; i <= n; i++) {
@@ -10,5 +10,30 @@ for (let i = 1; i <= n; i++) {
   }
   string += "\n";
 }
-// document.write(`${string}`);
 console.log(string);
+//2.square star pattern
+let num = 5;
+let str = "";
+for (let i = 1; i <= num; i++) {
+  for (let j = 1; j <= num; j++) {
+    str = str + "*";
+  }
+  str = str + "\n";
+}
+console.log(str, "square");
+// 3.hollow pattern
+let row = 5;
+let star = "";
+for (let i = 0; i < row; i++) {
+  for (let j = 0; j < row; j++) {
+    if (i === 0 || i === row - 1) {
+      star = star + "*";
+    } else if (j === 0 || j === row - 1) {
+      star = star + "*";
+    } else {
+      star = star + " ";
+    }
+  }
+  star = star + "\n";
+}
+console.log(star, "hollow pattern");
