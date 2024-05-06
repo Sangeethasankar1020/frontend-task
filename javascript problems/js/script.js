4492f71f89a3690137e190f709c627a6c8688079
Vikas <rmjvv31@gmail.com>
2024-05-06 09:44:10 +0530

    update

---
+++ javascript problems/js/script.js
@@ -0,0 +1,18 @@
+// Reverse a string
+function reverse(str) {
+    let reversedstr = str.split('').reverse().join('')
+    return reversedstr;
+}
+console.log(reverse("level"), "reversed str");
+
+// is palindrome or not
+function isPalindrome(str) {
+    let checkPalindrome = str.split('').reverse.join('')
+    if (str == checkPalindrome) {
+        console.log(isPalindrome,palindrome)
+    }
+    else {
+        console.log(isPalindrome,"is not palindrome")
+   }
+}
+console.log(isPalindrome(car))
