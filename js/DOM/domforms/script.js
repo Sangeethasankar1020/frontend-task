012c6ebbc864aa167f4bcde5bc7c560bc36e2903
Vikas <rmjvv31@gmail.com>
2024-05-14 16:45:26 +0530

    update

--- js/DOM/domforms/script.js
+++ js/DOM/domforms/script.js
@@ -1,1 +1,17 @@
 // form validation
+function myFunction() {
+    let x = document.getElementById("num").value;
+    let text;
+    if (isNaN(x) || x < 0 || x > 10) {
+        text = "Invalid Input";
+    } else if(x==""){
+        text = "Enter input";
+    }
+    else {
+        text=""
+    }
+    document.getElementById("error").innerHTML = text;
+}
+// css
+
+document.getElementById("heading").style.color = "blue";
