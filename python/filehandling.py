4e1112c516b8be337d19c07260e266ee1fcba2cc
Vikas <rmjvv31@gmail.com>
2024-05-14 14:26:38 +0530

    update

--- python/filehandling.py
+++ python/filehandling.py
@@ -1,27 +1,8 @@
 # file handling
-
-# we can read any file file python , txt
-# we use file handling to store like database
-f=open("tuple.py")
-print(f.read())
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content! good")
 f.close()
 
-# we can also read image file in binary format
-img=open("./src/sample.jpg","rb")
-print(img.read())
-img.close()
-
-#
-demo=open("./filehandling_sample_files/demo.txt","t+r")
-print(demo.read(5))
-demo.close()
-#
-demoReadline=open("./filehandling_sample_files/demo.txt","t+r")
-print(demoReadline.readline())
-print(demoReadline.readline())
-demoReadline.close()
-# we can also loop
-demoReadline = open("./filehandling_sample_files/demo.txt", "t+r")
-for x in demoReadline:
-    print(x)
-demoReadline.close()
+# open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
